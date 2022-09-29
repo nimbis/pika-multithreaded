81cb80de9ddfafc69d1faee97407f75b1acdc014
Ryan Stutzman <ryan.stutzman@nimbisservices.com>
2022-09-29 02:02:10 -0400

    Update example file to use multithreaded consumer
    This updates the example to work with the new multithreaded consumer.

--- examples/example.py
+++ examples/example.py
@@ -1,13 +1,11 @@
 # =================================
-# EXAMPLE SINGLE THREADED CONSUMER
+# EXAMPLE MULTITHREADED CONSUMER
 # =================================
 #      Author: Ryan Stutzman
-# Description: This is an example usage of a basic single threaded consumer
+# Description: This is an example usage of a basic multithreaded consumer
 # =================================
 import json
 import logging
-from pika.exceptions import ChannelClosedByBroker
-import signal
 
 from pika_multithreaded.clients import AmqpClient
 
@@ -16,7 +14,7 @@ logger = logging.getLogger(__name__)
 
 class ExampleMultithreadedFunctions:
     @staticmethod
-    def test_function_1(amqp_client, body, is_dry_run):
+    def test_function_1(body, is_dry_run):
         logger.debug("Entering 'test_function_1' function...")
         # Create a new message to send somewhere else
         amqp_message_data = {
@@ -29,7 +27,6 @@ class ExampleMultithreadedFunctions:
             }
         }
         ExampleMultithreadedFunctions.util_send_message(
-            amqp_client,
             is_dry_run,
             "",
             "test-queue-2",
@@ -37,7 +34,7 @@ class ExampleMultithreadedFunctions:
         )
 
     @staticmethod
-    def test_function_2(amqp_client, body, is_dry_run):
+    def test_function_2(body, is_dry_run):
         logger.debug("Entering 'test_function_2' function...")
         # Create a new message to send somewhere else
         amqp_message_data = {
@@ -50,7 +47,6 @@ class ExampleMultithreadedFunctions:
             }
         }
         ExampleMultithreadedFunctions.util_send_message(
-            amqp_client,
             is_dry_run,
             "",
             "test-queue-2",
@@ -67,14 +63,14 @@ class ExampleMultithreadedFunctions:
         logger.info(log_msg)
 
     @staticmethod
-    def util_send_message(amqp_client, is_dry_run, to_exchange, to_routing_key, body):
+    def util_send_message(is_dry_run, to_exchange, to_routing_key, body):
         if not is_dry_run:
             logger.debug(
                 f"Sending '{body}' to '{to_routing_key}' via '{to_exchange}'")
-            amqp_client.channel.basic_publish(
+            ExampleMultithreadedFunctions.new_amqp_client().send_message(
                 exchange=to_exchange,
                 routing_key=to_routing_key,
-                body=body
+                message=body
             )
         else:
             ExampleMultithreadedFunctions.util_log_amqp_message(
@@ -82,13 +78,22 @@ class ExampleMultithreadedFunctions:
                 to_routing_key,
                 body)
 
+    @staticmethod
+    def new_amqp_client():
+        return AmqpClient(
+            url="amqp://")
+
 
 class ExampleMultithreadedConsumer:
-    def __init__(self, queue="test-queue", debug=False, dry_run=False, broker_url="amqp://"):
+    ROUTING_TABLE = {
+        "test_function_1": ExampleMultithreadedFunctions.test_function_1,
+        "test_function_2": ExampleMultithreadedFunctions.test_function_2,
+    }
+
+    def __init__(self, queue="test-queue", debug=False, dry_run=False):
         self.is_dry_run = dry_run
         self.is_debug = debug
         self._amqp_queue = queue
-        self._amqp_broker_url = broker_url
 
     def run(self):
         if self.is_debug:
@@ -98,40 +103,22 @@ class ExampleMultithreadedConsumer:
             logger.addHandler(logging.StreamHandler())
             logger.setLevel(logging.DEBUG)
         logger.debug("============== STANDALONE SCRIPT DEBUG ==============")
-        logger.debug("Setting up SIGINT/SIGTERM handler...")
-        # Set up signal handlers since this script is intended to be run as its own process
-        signal.signal(signal.SIGINT, self.signal_handler)
-        signal.signal(signal.SIGTERM, self.signal_handler)
         # Connect to AMQP Broker
-        self.amqp_client = AmqpClient(url=self._amqp_broker_url)
+        self.amqp_client = ExampleMultithreadedFunctions.new_amqp_client()
+        # Set up signal handlers since this script is intended to be run as its own process
+        logger.debug("Setting up SIGINT/SIGTERM handler...")
+        self.amqp_client.setup_signal_handlers()
+        logger.debug(f"Establishing connection to AMQP Broker on queue '{self._amqp_queue}'")
         with self.amqp_client as amqp_client:
-            amqp_client.queue_declare(self._amqp_queue, durable=True)
-            keep_consuming = True
-            while keep_consuming:
-                logger.debug(f"Connecting to queue {self._amqp_queue}...")
-                try:
-                    amqp_client.consume(
-                        self._amqp_queue,
-                        self.receive_message,
-                        auto_ack=False)
-                    keep_consuming = False
-                except KeyboardInterrupt:
-                    # Keyboard interrupt for when a user hits Crtl+C
-                    keep_consuming = False
-                except ChannelClosedByBroker:
-                    # There is a timeout of 1800000 ms that results in this exception so catch the
-                    # exception and re-start the consumer
-                    logger.debug(
-                        "The consumer timed out (default is 1800000 ms). Restarting now...")
-                    keep_consuming = True
+            amqp_client.consume(
+                self._amqp_queue,
+                self.receive_message,
+                auto_ack=False,
+                declare_queue=True)
 
-    def signal_handler(self, sig, frame):
-        logger.warning("*** Emulator message service terminating. Closing AMQP connection...")
-        self.amqp_client.stop_consuming()
-
-    def receive_message(self, _, method, properties, body):
+    def receive_message(self, amqp_client, _, method, properties, body):
         logger.debug("****** Message Received! ******")
-        logger.debug(f"Channel: {self.amqp_client.channel}")
+        logger.debug(f"Channel: {amqp_client.channel}")
         logger.debug(f"Method: {method}")
         logger.debug(f"Properties: {properties}")
         logger.debug(f"Message: {body}")
@@ -142,7 +129,7 @@ class ExampleMultithreadedConsumer:
             # Acknowledge the message to kill it since we can't even read it
             # It may be best to set up a dead letter queue in case we want to review bad/ignored
             # messages at a later date, but for now we just kill them
-            self.amqp_client.channel.basic_nack(delivery_tag=method.delivery_tag, requeue=False)
+            amqp_client.nack_message_threadsafe(delivery_tag=method.delivery_tag, requeue=False)
             # We don't want to continue with processing this message if the message isn't in a
             # a format that we can actually process
             return
@@ -152,17 +139,13 @@ class ExampleMultithreadedConsumer:
             # Throw an error if the 'message_format' field is not provided
             if "message_format" not in msg_json:
                 raise Exception("Unknown message format: 'message_format' not provided")
-            # test_function_1
-            if msg_json["message_format"] == "test_function_1":
-                ExampleMultithreadedFunctions.test_function_1(
-                    self.amqp_client, msg_json["payload"], self.is_dry_run)
-            # test_function_2
-            elif msg_json["message_format"] == "test_function_2":
-                ExampleMultithreadedFunctions.test_function_2(
-                    self.amqp_client, msg_json["payload"], self.is_dry_run)
-            logger.debug("****** Message processed successfully!")
-            # Acknowledge the message
-            self.amqp_client.channel.basic_ack(delivery_tag=method.delivery_tag)
+
+            func = self.ROUTING_TABLE.get(msg_json["message_format"])
+            if func:
+                func(msg_json["payload"], self.is_dry_run)
+            logger.debug("****** Message processed successfully! Acking... ******")
+            amqp_client.ack_message_threadsafe(delivery_tag=method.delivery_tag)
+            logger.debug("****** Message acked successfully! ******")
         except Exception as ex:
             logger.error(f"ERROR: Unable to process message\n{ex}")
             # Acknowledge the message to kill it since we can't even read it
@@ -173,10 +156,10 @@ class ExampleMultithreadedConsumer:
             # We only want to requeue this if it is the original message and not requeued
             should_requeue = should_requeue and not method.redelivered
             if should_requeue:
-                self.amqp_client.channel.basic_nack(
+                amqp_client.nack_message_threadsafe(
                     delivery_tag=method.delivery_tag, requeue=True)
             else:
-                self.amqp_client.channel.basic_nack(
+                amqp_client.nack_message_threadsafe(
                     delivery_tag=method.delivery_tag, requeue=False)
 
 
